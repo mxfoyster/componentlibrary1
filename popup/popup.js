a503cfe0085be94a6003ed4bacd3a8196b1ec4bb
Mark Foyster <mxfoyster@yahoo.co.uk>
2022-07-03 18:28:38 +0100

    Tidied some leftover code from original application and added basic instructions to popup.js

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,34 @@
+/*
+    #############################################
+    #   Simple Pop Up Window v1.00              #
+    #       By Mark Foyster                     #
+    #############################################
+
+    MAKE A SUB DIRECTORY popup THEN ADD popup.js, popup.css & closebtn.png TO IT
+    THEN ADD THE FOLLOWING HTML WITHIN YOUR HEAD:
+    
+    <link rel="stylesheet" href="popup/popup.css">
+    <script src="popup/popup.js" defer></script>
+
+    
+    ADD THE FOLLOWING HTML WITHIN YOUR BODY (replacing content between ####'s with your own)
+
+    <div class="popUp" id="popUp">
+        <div class="popUpTitleContainer"><span class="popUpTitle">###PUT YOUR TITLE HERE####</span></div>
+        <img id="closeBtn" src="popup/closebtn.png">
+        #### PUT YOUR HTML FOR INSIDE THE POPUP HERE ####
+    </div>
+    
+    USE THE activatePopUp ID to your control to launch the product, EG:
+
+    <button type="button" id="activatePopUp">Pop Up</button>
+
+    
+
+
+*/
+
+
 const activatePopUpBtn = document.getElementById("activatePopUp");
 const popUpWindow = document.getElementById("popUp");
 const closeBtn = document.getElementById('closeBtn');
