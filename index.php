316f1b4714743e31c03fb553f3f5231aa973cb91
Mark Foyster <mxfoyster@yahoo.co.uk>
2022-07-14 13:03:17 +0100

    Added tooltip component to justcss

--- index.php
+++ index.php
@@ -35,13 +35,28 @@
             <p>Press the button to see the popup, code can be found in my GitHub repository <a href="https://github.com/mxfoyster/componentlibrary1/tree/main/popup">HERE</a></p>
             
             <button type="button" id="activatePopUp">Pop Up</button>
-
+            <hr/>
             <h3>Plain CSS Components</h3>
 
             <p>A collection of simple components that utilise only CSS. Many (probably all) of these will utilise CSS3 or beyond. Therefore please consider browser support. Find the code on my GitHub repository <a href="https://github.com/mxfoyster/componentlibrary1/tree/main/justcss">HERE</a></p>
 
             <h4>Simple Badge</h4>
+
+            <p>These badges are handy for focusing attention to something on the page.</p>
             <span class="badge">BADGE</span> <span class="badge">NEW</span> <span class="badge">SPECIAL OFFER</span>
+
+            <h4>Tooltips on mouse hover</h4>
+
+            <p>Hover over this 
+                     
+                    <span class="badge toolTipContainer">BADGE<span class="toolTipText">Here's the tooltip</span></span>
+            to see the tooltip. Or hover over this 
+                <span class="toolTipContainer">
+                    <span class="toolTipText">Here's another tooltip</span> 
+                    <span class="badge">BADGE</span>
+                </span>
+            to see a different tooltip. See how the same code works for multiple instances.</p>
+            <p>You will notice ther position of the tooltip depends on implementation.</p>
 
             <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
             <p>"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</p>
--- justcss/README.md
+++ justcss/README.md
@@ -14,14 +14,48 @@ If you wish to use several components, you could install the files on your serve
     <link rel="stylesheet" href="justcss/justcss.css">
 ```
 
-To utilise the component, simply add the appropriate class to the element you wish to use. For example, to utilise the **Badge** class:
+Now you will need to add the appropriate classes to your tags. Below are some specific examples.
+
+---
+
+#### BADGE
+Add the CSS to your project as described above.
+
+Now simply add the *badge* class to the element you wish to use like this:
 
 ```
     <div class="badge">MY TEXT</div>
 ```
 
-NOTE: As with all css, be aware that any default styling provided by the tag you choose may have to be overridden otherwise the componenet may not function correctly. Choosing `<span>` tags for inline elements and `<div>` for block level is advisable. 
+---
 
+#### Tool tips
+Add the CSS to your project as explained earlier. Now you have two options, let's add this component to a *badge* component to implement a tooltip. The easiest way is to add the `toolTipContainer` class to the *badge* element and then adding our tooltip inside it like so:
+
+```
+    <p>Testing a <span class="badge toolTipContainer">BADGE<span class="toolTipText">My tooltip</span></span> with hover.</p>
+```
+As you can see, the tooltip text is simply put within the `<span>` of class `toolTipText`. The tooltip will be displayed where we put it offset by the *margin* properties in our CSS.
+
+If a separate contatiner is used to encapsulate the *badge* and *toolTipText* containers like so:
+
+```
+    <span class="toolTipContainer">
+        <span class="toolTipText">Here's another tooltip</span> 
+        <span class="badge">BADGE</span>
+    </span>
+```
+
+The tooltip text will be display in the exact position of the *badge* element offset by the margins. Therefore, this method is more predictable. 
+
+To change the location of the tooltip, regardless of implementation method, simply alter the margins within the `.toolTipContainer .toolTipText ` selector of the CSS. You can play with the colours, borders etc here too! 
+
+---
+
+#### NOTE: 
+As with all css, be aware that any default styling provided by the tag you choose may have to be overridden otherwise the componenet may not function correctly. Choosing `<span>` tags for inline elements and `<div>` for block level is advisable. 
+
+----
 
 ### Customising style
 
